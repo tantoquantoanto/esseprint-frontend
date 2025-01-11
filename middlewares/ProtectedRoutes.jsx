5c3b3a13a05c80a30b7567f3aa95dbc34b9f6bfd
tantoquantoanto <antoniettaandreozzi91@gmail.com>
2025-01-11 14:10:02 +0100

    import corrected

--- middlewares/ProtectedRoutes.jsx
+++ middlewares/ProtectedRoutes.jsx
@@ -1,6 +1,6 @@
 
 import { Outlet } from "react-router-dom";
-import Login from "../src/pages/Login";
+import Login from "../src/Pages/Login";
 
 export const isAuthorized = () => {
   return (localStorage.getItem("Authorization"))
